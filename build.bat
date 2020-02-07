d5d831ce18e2ff5aaf2a85862eda113191318b0c
Ryzhehvost <kotlyar.andrey@gmail.com>
2020-02-07 12:55:41 +0200

    update dependencies

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 del .\BoosterCreator\*.zip
 dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\BoosterCreator\BoosterCreator.zip BoosterCreator-netf.zip 
-dotnet publish -c "Release" -f "netcoreapp3.0" -o "out/generic" "/p:LinkDuringPublish=false"
+dotnet publish -c "Release" -f "netcoreapp3.1" -o "out/generic" "/p:LinkDuringPublish=false"
