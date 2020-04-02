2cb2011693f98e8b9237733df52e0d71d86ca52c
Ryzhehvost <kotlyar.andrey@gmail.com>
2020-04-02 22:25:02 +0300

    update build script

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+if not exist ArchiSteamFarm\ArchiSteamFarm (git submodule update --init)
+git submodule foreach "git fetch origin; git checkout $(git rev-list --tags --max-count=1);"
 del .\BoosterCreator\*.zip
 dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\BoosterCreator\BoosterCreator.zip BoosterCreator-netf.zip 
