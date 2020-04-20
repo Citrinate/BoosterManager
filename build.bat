87bfba57c3ffa7819412db5c80fae390583d8f19
Ryzhehvost <kotlyar.andrey@gmail.com>
2020-04-20 18:53:23 +0300

    add custom version support to build sctipt

--- build.bat
+++ build.bat
@@ -1,5 +1,12 @@
+@echo off
 if not exist ArchiSteamFarm\ArchiSteamFarm (git submodule update --init)
+if [%1]==[] goto noarg
+git submodule foreach "git fetch origin; git checkout %1;"
+goto continue
+:noarg
 git submodule foreach "git fetch origin; git checkout $(git rev-list --tags --max-count=1);"
+:continue
+git submodule foreach "git describe --tags;"
 del .\BoosterCreator\*.zip
 dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\BoosterCreator\BoosterCreator.zip BoosterCreator-netf.zip 
